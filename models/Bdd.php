78d2f501b78986471fedfaa15b980708962cd1d4
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2022-06-15 17:54:04 +0200

    initial commit

---
+++ models/Bdd.php
@@ -0,0 +1,30 @@
+<?php
+
+abstract class Bdd
+{
+    private $host = 'localhost';
+    private $name ='al-beyt';
+    private $user ='root';
+    private $password = 'root';
+    protected $bdd;
+
+    public function __construct()
+    { 
+        try 
+        {
+            $this->bdd = new PDO('mysql:host='.$this->host.';dbname='.$this->name, $this->user,$this->password);
+            // Activation des erreurs PDO
+            $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // mode de fetch par défaut : FETCH_ASSOC / FETCH_OBJ / FETCH_BOTH
+            $this->bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        }
+        catch(PDOException $e)
+        {
+            die('Erreur : ' . $e->getMessage());
+        }
+    }
+
+}
+
+
+
