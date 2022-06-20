e63ea2bdbe4f6ae2d9635e781d41ca14420da3f6
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2022-06-20 13:05:16 +0200

    refactor requete getAllEvent

--- models/EvenementModel.php
+++ models/EvenementModel.php
@@ -3,13 +3,16 @@
 require_once('Bdd.php');
 class EvenementModel extends Bdd {
 
-
-
-  
-
-    public function getAllEvent(){
-         
-        $bdd = $this->bdd->prepare('SELECT * from evenement INNER JOIN artiste_evenement on evenement.id = artiste_evenement.id_evenement INNER JOIN artiste ON artiste_evenement.id_artiste = artiste.id INNER JOIN image_evenement ON image_evenement.id_evenement = evenement.id  ');
+    public function getAllEvent()
+    {
+        $bdd = $this->bdd->prepare(
+        'SELECT * from evenement 
+        INNER JOIN artiste_evenement
+        ON evenement.id = artiste_evenement.id_evenement
+        INNER JOIN artiste
+        ON artiste_evenement.id_artiste = artiste.id
+        INNER JOIN image_evenement
+        ON image_evenement.id_evenement = evenement.id');
         $bdd->execute();
         $result = $bdd->fetch();
 
