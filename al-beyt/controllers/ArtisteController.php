f7bb4217ca86a465e31c986fc5e38f05d90d5bfe
SarrD <leroidave@gmail.com>
2022-07-07 13:06:41 +0200

    condition sur la pagination des requetes displayArtistes

--- al-beyt/controllers/ArtisteController.php
+++ al-beyt/controllers/ArtisteController.php
@@ -6,6 +6,7 @@ use AlBeyt\Models\EvenementModel;
 class ArtisteController{
 
     protected $modelArtiste;
+    protected $modelEvenement;
     const NBR_ARTISTE_PAR_PAGE = 10;
     const NBR_EVENEMENT_PAGE_ARTISTE = 4;
     
@@ -16,19 +17,30 @@ class ArtisteController{
         $this->modelEvenement = new EvenementModel();
     }
 
-    public function displayAllArtists($pageCourante)
-    {   
-        $limit = self::NBR_ARTISTE_PAR_PAGE;
-        $offset = self::NBR_ARTISTE_PAR_PAGE * ($pageCourante-1);
-        $displayAllArtists = $this->modelArtiste->getAllArtists($limit,$offset);
+    public function displayAllArtists($pageCourante = null)
+    {
+        if($pageCourante != null){
+            $limit = self::NBR_ARTISTE_PAR_PAGE;
+            $offset = self::NBR_ARTISTE_PAR_PAGE * ($pageCourante - 1);
+            $displayAllArtists = $this->modelArtiste->getAllArtists($limit, $offset);
+        }else{
+            $displayAllArtists = $this->modelArtiste->getAllArtists(100000, 0);
+
+        }
         return $displayAllArtists;
+
     }
     
-    public function displayAllArtistsByDomain($id_domain,$pageCourante)
-    { 
-        $limit = self::NBR_ARTISTE_PAR_PAGE;
-        $offset = self::NBR_ARTISTE_PAR_PAGE * ($pageCourante-1);
-        $displayAllArtistsByDomain = $this->modelArtiste->getAllArtistsByDomain($id_domain,$limit,$offset);
+    public function displayAllArtistsByDomain($id_domain,$pageCourante = null)
+    {
+        if($pageCourante != null){
+            $limit = self::NBR_ARTISTE_PAR_PAGE;
+            $offset = self::NBR_ARTISTE_PAR_PAGE * ($pageCourante - 1);
+            $displayAllArtistsByDomain = $this->modelArtiste->getAllArtistsByDomain($id_domain, $limit, $offset);
+        }else{
+            $displayAllArtistsByDomain = $this->modelArtiste->getAllArtistsByDomain($id_domain, 100000, 0);
+
+        }
         return $displayAllArtistsByDomain;
     }
     
@@ -40,9 +52,13 @@ class ArtisteController{
 
     public function displayEventsByIdArtist($id_artiste,$pageCourante)
     {
-        $limit = self::NBR_EVENEMENT_PAGE_ARTISTE;
-        $offset = self::NBR_EVENEMENT_PAGE_ARTISTE * ($pageCourante-1);
-        $displayEventsByIdArtist = $this->modelEvenement->getEventsByIdArtist($id_artiste,$limit,$offset);
+        if($pageCourante != null){
+            $limit = self::NBR_EVENEMENT_PAGE_ARTISTE;
+            $offset = self::NBR_EVENEMENT_PAGE_ARTISTE * ($pageCourante - 1);
+            $displayEventsByIdArtist = $this->modelEvenement->getEventsByIdArtist($id_artiste, $limit, $offset);
+        }else{
+            $displayEventsByIdArtist = $this->modelEvenement->getEventsByIdArtist($id_artiste, 100000, 0);
+        }
         return $displayEventsByIdArtist;
     }
 
