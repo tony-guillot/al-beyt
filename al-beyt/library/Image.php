0785f7bf0584324f29f2e205304104162177ea97
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2022-07-11 17:21:49 +0200

    modify constante extensions -> types

--- al-beyt/library/Image.php
+++ al-beyt/library/Image.php
@@ -5,14 +5,14 @@ namespace AlBeyt\Library;
 class Image
 {
     const SOUS_REPERTOIRE_IMAGES = '/../../images/';
-    const EXTENSIONS_AUTORISEES = ['image/jpeg','image/png','image/gif'];
+    const TYPES_AUTORISES = ['image/jpeg','image/png','image/gif'];
 
 
     public static function sauvegardeImage($image)
     {
         $cheminDestination = "";
         if(isset($image) && $image['error'] == UPLOAD_ERR_OK){
-            if(in_array($image['type'], self::EXTENSIONS_AUTORISEES)){
+            if(in_array($image['type'], self::TYPES_AUTORISES)){
                 $nomFichier = str_replace(" ","",$image['name']); //pour eviter les espaces dans le chemin
                 //On construit le chemin absolu du dossier dans lequel on va mettre le fichier + nom du fichier (horodaté)
                 $cheminDestination = realpath(__DIR__.self::SOUS_REPERTOIRE_IMAGES).DIRECTORY_SEPARATOR.time()."_".$nomFichier;
