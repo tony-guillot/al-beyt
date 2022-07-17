337e6b34132951b8dd862ae048484486f88e0c8e
SarrD <leroidave@gmail.com>
2022-07-17 17:37:44 +0200

    refacto : print slider articles dans classe Affichage

--- al-beyt/library/Affichage.php
+++ al-beyt/library/Affichage.php
@@ -44,6 +44,57 @@ class Affichage
         return $displayTags;
     }
 
+    public static function printImageSliderForArticles($images_article)
+    {
+        $displayTags = '';
+        if (isset($images_article[1]))
+        {
+            $displayTags .= '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+                    <div id="sliderImages" class="carousel carousel-dark" data-ride="carousel" >
+                          <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#sliderImages" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#sliderImages" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#sliderImages" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                          </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="http://'. $images_article[1]['chemin'] .'" alt="'. $images_article[1]['legende'] .'" >
+                                <div class="carousel-caption d-block">
+                                    <h5>'. $images_article[1]['legende'] .'</h5>
+                                </div>
+                            </div>';
+           if(isset($images_article[2]))
+           {
+                $displayTags .= '<div class="carousel-item">
+                                    <img src="http://'. $images_article[2]['chemin'] .'" alt="'. $images_article[2]['legende'] .'">
+                                    <div class="carousel-caption d-block">
+                                        <h5>'. $images_article[2]['legende'] .'</h5>
+                                    </div>
+                                </div>';
+           }
+           if(isset($images_article[3]))
+           {
+                $displayTags .= '<div class="carousel-item">
+                                    <img src="http://'. $images_article[3]['chemin'] .'" alt="'. $images_article[3]['legende'] .'">
+                                    <div class="carousel-caption d-block">
+                                        <h5>'. $images_article[3]['legende'] .'</h5>
+                                    </div>
+                                </div>';
+           }
+               $displayTags .= '</div>
+                                  <button class="carousel-control-prev" type="button" data-bs-target="#sliderImages" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                  </button>
+                                  <button class="carousel-control-next" type="button" data-bs-target="#sliderImages" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                  </button>
+                            </div>';
+        }
+        return $displayTags;
+    }
+
     public function printAllArtists()
     {
 
--- al-beyt/views/front/article.php
+++ al-beyt/views/front/article.php
@@ -2,6 +2,8 @@
 require_once '../../../vendor/autoload.php';
 require_once('../include/header.php');
 use AlBeyt\Controllers\ArticleController;
+use AlBeyt\Library\Affichage;
+
 $controllerArticle = new ArticleController();
 
 if(isset($_GET['id'])){
@@ -26,14 +28,8 @@ $images_article = $controllerArticle->displayImagesByIdArticle($id);
         </section>
 
         <section>
-
-
-
-
-
             <!-- <article>
                 <p>
-
                 </p>
             </article> -->
             <article>
@@ -42,59 +38,7 @@ $images_article = $controllerArticle->displayImagesByIdArticle($id);
                         <?= $article['description'] ?>
                     </p>
                 </div>
-
-               <?php if (isset($images_article[1])): ?>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-                    <div id="sliderImages" class="carousel carousel-dark" data-ride="carousel" >
-                          <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#sliderImages" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#sliderImages" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#sliderImages" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                          </div>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="http://<?= $images_article[1]['chemin'] ?>" alt="<?= $images_article[1]['legende'] ?>" >
-                                <div class="carousel-caption d-block">
-                                    <h5><?= $images_article[1]['legende'] ?></h5>
-                                </div>
-                            </div>
-                            <?php if (isset($images_article[2])): ?>
-                                <div class="carousel-item">
-                                    <img src="http://<?= $images_article[2]['chemin'] ?>" alt="<?= $images_article[2]['legende'] ?>">
-                                    <div class="carousel-caption d-block">
-                                        <h5><?= $images_article[2]['legende'] ?></h5>
-                                    </div>
-                                </div>
-                            <?php endif ?>
-                            <?php if (isset($images_article[3])): ?>
-                                <div class="carousel-item">
-                                    <img src="http://<?= $images_article[3]['chemin'] ?>" alt="<?= $images_article[3]['legende'] ?>">
-                                   <div class="carousel-caption d-block">
-                                        <h5><?= $images_article[3]['legende'] ?></h5>
-                                    </div>
-                                </div>
-                            <?php endif ?>
-                        </div>
-                          <button class="carousel-control-prev" type="button" data-bs-target="#sliderImages" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                          </button>
-                          <button class="carousel-control-next" type="button" data-bs-target="#sliderImages" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                          </button>
-                    </div>
-                <?php endif ?>
-
-
-                <!-- <div>
-                  <img src="<?= $images_article[1]['chemin'] ?>" alt="<?= $images_article[1]['legende'] ?>">
-                  <img src="<?= $images_article[2]['chemin'] ?>" alt="<?= $images_article[2]['legende'] ?>">
-                  <img src="<?= $images_article[3]['chemin'] ?>" alt="<?= $images_article[3]['legende'] ?>">
-                    <p>
-
-                    </p>
-                </div>-->
+                <?php echo Affichage::printImageSliderForArticles($images_article); ?>
             </article>
         </section>
     </section>
