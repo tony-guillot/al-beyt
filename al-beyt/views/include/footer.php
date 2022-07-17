54013df4ecda56ec7ff82cdd06961c374d4706c5
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2022-07-17 18:38:41 +0200

    footer html ok

--- al-beyt/views/include/footer.php
+++ al-beyt/views/include/footer.php
@@ -1,5 +1,23 @@
-<footer>
-    
-</footer>
+    <footer>
+        <section>
+            <article>
+                <img src="../../../images/logo.png" alt="logo al-beyt">
+            </article>
+            <article>
+                <h1>Contact</h1>
+                <a href="mailto:collectif.albeyt@gmail.com">collectif.albeyt@gmail.com</a>
+            </article>
+            <article>
+                <a href="">Mentions égales</a>
+            </article>
+            <article>
+                    <div>
+                        <a href="https://www.instagram.com/al_beyt_collectif/"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="https://www.facebook.com/Collectif-AL-Beyt-103326081984889"><i class="fa-brands fa-facebook"></i></a>
+                        <a href="mailto:collectif.albeyt@gmail.com"> <i class="fa-solid fa-envelope"></i></a>
+                    </div>
+            </article>
+        </section>
+    </footer>
 </body>
 </html>
