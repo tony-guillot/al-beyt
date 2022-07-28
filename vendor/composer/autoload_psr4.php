187c78f3acaa0d33ddeb2142dea534009549972d
SarrD <leroidave@gmail.com>
2022-07-28 19:00:55 +0200

    feature : telechargement de l'article au format pdf

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,11 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Svg\\' => array($vendorDir . '/phenx/php-svg-lib/src/Svg'),
+    'Sabberworm\\CSS\\' => array($vendorDir . '/sabberworm/php-css-parser/src'),
+    'Masterminds\\' => array($vendorDir . '/masterminds/html5/src'),
+    'FontLib\\' => array($vendorDir . '/phenx/php-font-lib/src/FontLib'),
+    'Dompdf\\' => array($vendorDir . '/dompdf/dompdf/src'),
     'AlBeyt\\Views\\' => array($baseDir . '/al-beyt/views'),
     'AlBeyt\\Models\\' => array($baseDir . '/al-beyt/models'),
     'AlBeyt\\Library\\' => array($baseDir . '/al-beyt/library'),
