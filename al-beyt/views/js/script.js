f5c74bd634dbdc490f7b2609a904c8e0bf0cd45d
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2022-08-01 17:56:53 +0200

    commentaire sur script.js

--- al-beyt/views/js/script.js
+++ al-beyt/views/js/script.js
@@ -1,5 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    //Gestion des erreurs 
+    if(typeof error !== "undefined")
+    {
+        let errorTag = document.getElementById('error-text');
+        let textError = document.createTextNode(error);
+        errorTag.append(textError);
+        let errorSection = document.getElementById('error-section');
+        errorSection.style.display = "flex";
+    }
+    
+
+     // Pagination 
+        var page = 1;
+        loadActu(1);
+
+        let prevButton = document.getElementById('actu-prev');
+        let nextButton = document.getElementById('actu-next');
+
+        prevButton.addEventListener("click", function () {
+            if (page > 0) {
+                page--;
+                loadActu(page);
+            }
+        });
+        nextButton.addEventListener("click", function () {
+            page++;
+            loadActu(page);
+        })
+
+    // Parralax
     let images = document.getElementsByClassName('parallax');
     for (var i =0; i < images.length; i++) {
         new simpleParallax(images, {
@@ -7,34 +37,7 @@ document.addEventListener("DOMContentLoaded", function() {
             scale : 2,
             orientation : "down",
         });
-    }
-/************************************************************************* */
-    if(typeof error !== "undefined"){
-        let errorTag = document.getElementById('error-text');
-        let textError = document.createTextNode(error);
-        errorTag.append(textError);
-        let errorSection = document.getElementById('error-section');
-        errorSection.style.display = "flex";
-    }
-
-
-/************************************************************************* */
-    var page = 1;
-    loadActu(1);
-
-    let prevButton = document.getElementById('actu-prev');
-    let nextButton = document.getElementById('actu-next');
-
-    prevButton.addEventListener("click", function () {
-        if (page > 0) {
-            page--;
-            loadActu(page);
-        }
-    });
-    nextButton.addEventListener("click", function () {
-        page++;
-        loadActu(page);
-    })
+    }    
 
 });
 
